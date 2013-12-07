97b73047950102e5caa9a7f178968ddc7ef0bc0c
Scatman <simon.fuentes@alumnos.usm.cl>
2013-12-07 11:52:04 -0300

    login y menu principal

--- ProyectoM/src/com/proyectom/MainActivity.java
+++ ProyectoM/src/com/proyectom/MainActivity.java
@@ -1,5 +1,9 @@
 package com.proyectom;
 
+
+
+import com.example.proyectom.R;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -7,20 +11,38 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+//import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	Button Registrar;
+	private Button Registrar;
+	private EditText PassEdit;
+	private EditText NameEdit;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        PassEdit = (EditText) findViewById(R.id.passEdit);
+        NameEdit = (EditText) findViewById(R.id.nameEdit);
         Registrar = (Button) findViewById(R.id.button1);
+        
+        
         Registrar.setOnClickListener(new OnClickListener() {
         	
         	public void onClick(View v){
-        		Intent intent = new Intent(getApplicationContext(),RegisterTravel.class);
+        	//login provisional	
+//        	if(NameEdit.getText().toString().equals(getString(R.string.name))
+//        			&& PassEdit.getText().toString().equals(getString(R.string.pass))){
+//  
+        		Intent intent = new Intent(getApplicationContext(), MenuPrincipal.class);
         		startActivity(intent);
+        		finish();
+        		
+//        	} else {
+//    			Toast.makeText(getApplicationContext(), getString(R.string.error_message), Toast.LENGTH_SHORT).show();
+//    		}
         	}
         });
     }
--- ProyectoM/src/com/proyectom/RegisterTravel.java
+++ ProyectoM/src/com/proyectom/RegisterTravel.java
@@ -1,12 +1,14 @@
 package com.proyectom;
 
 import java.util.Vector;
+import com.example.proyectom.R;
 
-import android.os.Bundle;
+
+
 import android.app.Activity;
-import android.util.Log;
-import android.view.Menu;
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
